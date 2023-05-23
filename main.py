4e2276d496884211bbb2bf8a1b7897d0b6372d30
emad947 <134236202+emad947@users.noreply.github.com>
2023-05-23 03:49:36 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def on_gesture_shake():
-    basic.show_number(randint(1, 9))
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+def on_button_pressed_a():
+    basic.show_leds("""
+    . . # . .
+    . # . # .
+    # # # # #
+    # . . . #
+    # . . . #
+    """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    basic.showNumber(randint(1, 9))
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showLeds(`
+    . . # . .
+    . # . # .
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
 })
